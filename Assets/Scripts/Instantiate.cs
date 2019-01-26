5fff0363fa8059aeedd2abbf095eec47eb904322
Ricardo Lázaro <caffeine305@gmail.com>
2019-01-26 06:51:49 -0600

    Added candles and its behaviour!

--- Assets/Scripts/Instantiate.cs
+++ Assets/Scripts/Instantiate.cs
@@ -12,11 +12,16 @@ public class Instantiate : MonoBehaviour {
 
     IEnumerator Cargar() {
 
-        Vector3 posicion1 = new Vector3(-7.0f, 4.0f, 0.0f);
-        Vector3 posicion2 = new Vector3(6.0f, 4.0f, 0.0f);
+        Vector3 posicion1 = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 posicion2 = new Vector3(0.0f, 0.0f, 16.0f);
+        Vector3 posicion3 = new Vector3(16.0f, 0.0f, 16.0f);
+        Vector3 posicion4 = new Vector3(16.0f, 0.0f, 0.0f);
+
 
         Instantiate(flama, posicion1, transform.rotation);
         Instantiate(flama, posicion2, transform.rotation);
+        Instantiate(flama, posicion3, transform.rotation);
+        Instantiate(flama, posicion4, transform.rotation);
 
         yield return new WaitForSeconds(seconds: 0.0f);
     }
