1164398292ce6c6344739df143ad1fd52558131f
Ricardo Lázaro <caffeine305@gmail.com>
2019-01-26 04:01:44 -0600

    WhiteBoxes - Player, Lights, Source
    Player has movement, Lights can be re-powered, source can be of use.

---
+++ Assets/Scripts/flameBehave.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flameBehave : MonoBehaviour {
+
+	// Use this for initialization
+
+public GameObject player;
+
+    int maxHP = 1000;
+    public int actualHP;
+    public float fixedtime;
+
+    void Start()
+    {
+        fixedtime = 1f;
+        this.actualHP = maxHP;
+        StartCoroutine(Turnlight());
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            this.actualHP = maxHP;
+            Debug.Log("Touching flame");
+        }
+    }
+
+    IEnumerator Turnlight()
+    {
+        while (actualHP > 0)
+        {
+            actualHP = actualHP - 1;
+            Debug.Log("flame: " + actualHP);
+            yield return new WaitForSeconds(fixedtime);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/onTouch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onTouch : MonoBehaviour {
+
+    int maxHP = 1000;
+    public int actualHP;
+    public float fixedtime;
+
+    public GameObject fuente;
+    
+    void Start () {
+        fixedtime = 1f;
+        this.actualHP = maxHP;
+        StartCoroutine(Lowerlight());
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Source")
+        {
+            this.actualHP = maxHP;
+            Debug.Log("Touching!");
+
+        }
+    }
+
+    IEnumerator Lowerlight()
+    {
+        while (actualHP > 0)
+        {
+            actualHP = actualHP -1;
+            yield return new WaitForSeconds(fixedtime);
+        }
+
+    
+    }
+
+}
