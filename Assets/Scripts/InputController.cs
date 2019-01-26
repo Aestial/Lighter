edd7b8cd1cc03e47edbc2f161e00f41b7c40f1aa
Jaime Hernandez <jaimevazz@gmail.com>
2019-01-26 01:12:45 -0600

    Added Rewired and basic Input

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+
+public class InputController : MonoBehaviour 
+{
+    [SerializeField] private float speedMultiplier = 4f;
+
+    public Rewired.Player rp { get { return ReInput.isReady ? ReInput.players.GetPlayer(0) : null; } }
+    private Rigidbody rb;
+
+    private float speed;
+
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        
+        if (!ReInput.isReady) return; // Exit if Rewired isn't ready. This would only happen during a script recompile in the editor.
+        this.speed = this.speedMultiplier;
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = rp.GetAxis("Move Horizontal"); // get input by name or action id
+        moveVector.z = rp.GetAxis("Move Vertical");
+        bool fire = rp.GetButtonDown("Fire");
+
+        if (moveVector != Vector3.zero)
+        {
+            this.rb.velocity = (moveVector * this.speed);
+            this.transform.rotation = Quaternion.LookRotation(moveVector);
+        }
+        if (rp.GetButtonDown("Fire"))
+        {
+            // Do Shit
+        }
+    }
+
+}
