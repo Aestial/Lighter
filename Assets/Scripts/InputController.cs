80b2369f4ca5e0df7e2c27931a22468a0c7b46be
Jaime Hernandez <jaimevazz@gmail.com>
2019-01-27 13:57:00 -0600

    Added Audio loops to screen scenes and Fire safe at start on Main

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -16,40 +16,53 @@ public class InputController : MonoBehaviour
 
     private bool isFiring;
 
+    private bool canFire;
+
     void Start()
     {
         this.rb = GetComponent<Rigidbody>();
         this.inputDebugger = GetComponent<InputDebugger>();
+        this.canFire = false;
+        this.isFiring = false;
+        StartCoroutine(EnableFireCoroutine());
     }
 
     void FixedUpdate()
     {
-        
+      
         if (!ReInput.isReady) return; // Exit if Rewired isn't ready. This would only happen during a script recompile in the editor.
         this.speed = this.isFiring ? this.speedMultiplier * 2.0f : this.speedMultiplier;
         Vector3 moveVector = Vector3.zero;
         moveVector.x = rp.GetAxis("Move Horizontal"); // get input by name or action id
         moveVector.z = rp.GetAxis("Move Vertical");
-        bool fire = rp.GetButtonDown("Fire");
 
         if (moveVector != Vector3.zero)
         {
             this.rb.velocity = (moveVector * this.speed);
             this.transform.rotation = Quaternion.LookRotation(moveVector);
         }
-        if (rp.GetButtonDown("Fire"))
-        {
-            // Do Shit
-            this.isFiring = true;
-            this.inputDebugger.Fire(this.isFiring);
 
-        }
-        else if (rp.GetButtonUp("Fire"))
+        if (canFire)
         {
-            // Stop Shit
-            this.isFiring = false;
-            this.inputDebugger.Fire(this.isFiring);
+            if (rp.GetButtonDown("Fire"))
+            {
+                // Do Shit
+                this.isFiring = true;
+                this.inputDebugger.Fire(this.isFiring);
+
+            }
+            else if (rp.GetButtonUp("Fire"))
+            {
+                // Stop Shit
+                this.isFiring = false;
+                this.inputDebugger.Fire(this.isFiring);
+            } 
         }
     }
 
+    IEnumerator EnableFireCoroutine()
+    {
+        yield return new WaitForSeconds(0.25f);
+        this.canFire = true;
+    }
 }
