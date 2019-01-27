3729bdacc547379f28a199487a194b76db92e83c
Ricardo Lázaro <caffeine305@gmail.com>
2019-01-27 03:20:26 -0600

    Sistema de lógica de pantallas del juego

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            SceneManager.LoadScene("MainRicardo");
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Return.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Return : MonoBehaviour {
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            SceneManager.LoadScene("Title");
+        }
+
+    }
+
+}
