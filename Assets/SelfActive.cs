8db6dda4498576d15e0fa5e57672d4e24c1b6b33
Jaime Hernández <jaimevazz@gmail.com>
2019-02-17 16:38:42 -0600

    Rewired update and Joystick detect
    - Updated Unity.
    - Updated Rewired.
    - Joystick detect (for UI).

---
+++ Assets/SelfActive.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfActive : MonoBehaviour
+{
+    [SerializeField] bool setState;
+    [SerializeField] float delay;
+
+    void OnEnable()
+    {
+        StartCoroutine(DelayAndActive(setState, delay));
+    }
+
+    IEnumerator DelayAndActive(bool state, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(state);
+    }
+}
