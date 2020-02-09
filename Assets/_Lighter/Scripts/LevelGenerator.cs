5031c70d01799e7c02e68e952f5baf1b1adddd94
Jaime Hernandez <jaimevazz@gmail.com>
2020-02-09 13:06:00 -0600

    First Level Generator: Read from texture

---
+++ Assets/_Lighter/Scripts/LevelGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+	[SerializeField] Texture2D map;
+    void Start()
+    {
+		GenerateLevel();
+    }
+    void GenerateLevel()
+	{
+        for (int x = 0; x < map.width; x++)
+		{
+            for (int y = 0; y < map.height; y++)
+			{
+				GenerateTile(x, y);
+			}
+		}
+	}
+    void GenerateTile(int x, int y)
+	{
+		Color pixelColor = map.GetPixel(x, y);
+        if (pixelColor.a == 0)
+		{
+			// Ignore transparent pixels
+			return;
+		}
+		Debug.Log(pixelColor);
+	}
+}
