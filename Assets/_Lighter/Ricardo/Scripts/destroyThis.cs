434b2798fc79801b6bbec3e361ee09b99517e3bf
Ricardo Lázaro <caffeine305@gmail.com>
2020-03-07 11:14:22 -0600

    Added some wind behaviour (wind current is destroyed after use)

---
+++ Assets/_Lighter/Ricardo/Scripts/destroyThis.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyThis : MonoBehaviour
+{
+    public void OnTriggerEnter() 
+    {
+        Destroy(this.transform.parent.gameObject);
+    }
+
+}
