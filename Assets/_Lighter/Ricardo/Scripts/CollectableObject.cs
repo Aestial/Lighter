5400697ccc9d9ff3370cbec7e36ec8953328dbcb
Ricardo Lázaro <caffeine305@gmail.com>
2020-03-12 22:27:30 -0600

    Added some Collectable Behaviour
    I'm trying to "read" data while the collectible is collided, but I'm getting kinda confused...

---
+++ Assets/_Lighter/Ricardo/Scripts/CollectableObject.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CollectableObject : MonoBehaviour, IBreakable, ICollectable<int>
+{
+ [SerializeField] int contador = 0;
+ public int puntos;
+ public int point;
+
+    public void Break()
+    {
+        Destroy(this.gameObject);
+    }
+
+//Quizá hay que mover este al contador
+    public void PointCount(int point)
+    {
+        contador = puntos + point;
+        Break();
+    }
+
+    public int getPoints()
+    {
+        return puntos;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            PointCount(1);
+            Debug.Log("Points: "+ contador);
+            getPoints();
+        }
+    }
+
+    void Start()
+    {
+        puntos = contador;
+    }
+
+}
---
+++ Assets/_Lighter/Ricardo/Scripts/ICollectable.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public interface ICollectable<T>
+{
+    void PointCount(T point);
+}
